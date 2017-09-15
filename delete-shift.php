8b249f95d6cd4dd32b5180cf6f8e6a014c114dbd
ParkerH96 <parker.a.householder@gmail.com>
2017-09-15 10:40:06 -0400

    Fixed deletion error

--- delete-shift.php
+++ delete-shift.php
@@ -5,16 +5,16 @@
   if(isset($_GET['shift_id']) && !empty($_GET['shift_id']) && $admin_tag == 1){
 
     $shift_id = $mysqli->escape_string($_GET['shift_id']);
-    $week = $mysqli->escape_string($_POST['week']);
-    $month = $mysqli->escape_string($_POST['month']);
-    $day = $mysqli->escape_string($_POST['day']);
+    $week = $mysqli->escape_string($_GET['week']);
+    $month = $mysqli->escape_string($_GET['month']);
+    $day = $mysqli->escape_string($_GET['day']);
 
     $mysqli->query("DELETE FROM SHIFT WHERE shift_id = $shift_id");
 
-    if(!$week) {
+    if(isset($week) && !empty($week)) {
       $get_location = 'week='.$week;
     }
-    else if(!$month){
+    else if(isset($month) && !empty($month)){
       $get_location = 'month='.$month;
     }
     else {
